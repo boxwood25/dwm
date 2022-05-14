dc7206df9ee72b68733cd71e95c9496c8cc52ff6
Lionel <lionel.busboom@gmx.de>
2022-05-14 13:36:36 +0200

    First attempt at making dwm suitable for KDE.
    Edit: Removed dwm bar.

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -29,7 +29,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+        { NULL,       NULL, "Desktop - Plasma", (1 << 9) - 1,  1,        -1 },
+        { "plasmashell", NULL,    NULL,       (1 << 9) - 1,  1,          -1 },
+        { "Plasma",   NULL,       NULL,       (1 << 9) - 1,  1,          -1 },
+        { NULL,       NULL, "plasma-desktop", (1 << 9) - 1,  1,          -1 },
 };
 
 /* layout(s) */
