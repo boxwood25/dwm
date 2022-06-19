e0aae5f7c1f3f139b7c6cc0ee872559d2a64ccab
Lionel <lionel.busboom@gmx.de>
2022-06-19 22:36:58 +0200

    Added MOD+shift+k for closing the window / killing the client.
    MOD+shift+c still works as well, but I decided
    to also have a one-handed keybinding for closing
    windows.

--- config.def.h
+++ config.def.h
@@ -126,6 +126,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_k,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
