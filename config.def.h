cb4b57402ffec5ac1855d95b62c6a88637b7527b
Lionel <lionel.busboom@gmx.de>
2022-06-28 22:32:30 +0200

    Changed keybindings.
    Setting the binary tile layout now requires no
    shift, toggling the border does.

--- config.def.h
+++ config.def.h
@@ -124,7 +124,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -143,7 +143,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_x,      killclient,     {0} },
-        { MODKEY|ShiftMask,		XK_b,	   setlayout,	   {.v = &layouts[0]} },
+        { MODKEY,		        XK_b,	   setlayout,	   {.v = &layouts[0]} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[3]} },
